0c564804621141f62dd6d344ad0fba2c5310d364
- <->
2022-11-19 19:28:10 +0300

    Upd Task3

--- hw2-app/src/components/Task3/LifeCycle.js
+++ hw2-app/src/components/Task3/LifeCycle.js
@@ -1,7 +1,33 @@
 import React from "react";
 
 class LifeCycle extends React.Component {
+    constructor(props) {
+       super(props);
+        this.state = {
+            page: 'Home',
+            title: 'Home page'
+        };
+    }
 
+    componentDidUpdate(prevState) {
+        if(this.state.title !== prevState){
+            this.setState({title: 'Contacts page'})
+        }
+    }
+
+    handleClick = (event) => {
+        event.preventDefault();
+        this.setState({page: 'Contacts'})
+    }
+
+    render(){
+        return(
+            <div>
+                <h1>{this.state.title}</h1>
+                <button onClick={this.handleClick}>Push me</button>
+            </div>
+        )
+    }
 }
 
 export default LifeCycle;
