e45a949b95d417b5548e01c90220c4131607329d
- <->
2022-11-19 18:11:13 +0300

    Upd Task 2

--- hw2-app/src/components/Task2/Form.js
+++ hw2-app/src/components/Task2/Form.js
@@ -7,16 +7,63 @@ state = {
     error: ''
 }
 
+onSubmit = (event) => {
+    event.preventDefault();
+
+    if((!this.state.name) || (!this.state.surname)) {
+        this.setState({
+            error: 'Введіть свої дані'
+        })
+    }else if ((this.state.name.length < 3) || (this.state.surname.length < 3)) {
+        this.setState({
+            error: 'З вашими даними щось пішло не так'
+        })
+    }else{
+        this.setState({
+            error: `Вітаємо, ${this.state.name} ${this.state.surname}`,
+            name: '',
+            surname: ''
+        });
+    }
+
+    console.log(this.state.name, this.state.surname)
+};
+
+changeNameValue = (event) => {
+    this.setState({name: event.target.value});
+
+    this.setState({
+        error: ''
+    })
+}
+
+changeSurnameValue = (event) => {
+    this.setState({surname: event.target.value});
+
+    this.setState({
+        error: ''
+    })
+};
+
+
 render() {
 
     return(
         <div>
-            <form>
+            <form onSubmit={this.onSubmit}>
                 <div>
-                    <input placeholder='Name' name='name' value={this.state.name} />
+                    <input 
+                    placeholder='Name' 
+                    name='name' 
+                    value={this.state.name}
+                    onChange={this.changeNameValue} />
                 </div>
                 <div>
-                    <input placeholder='Surname' name='surname' />
+                    <input 
+                    placeholder='Surname' 
+                    name='surname'
+                    value={this.state.surname} 
+                    onChange={this.changeSurnameValue}/>
                 </div>
                 <button type='submit'>Submit</button>
             </form>
